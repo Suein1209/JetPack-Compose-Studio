52ee03e64d251d3db09c6702ffff39bbe6d88379
김병진 <suein1209@wemakeprice.com>
2023-03-28 22:37:52 +0900

    모션 애니메이션, 컬러애니메이션

--- app/src/main/java/com/suein/myapplication/jpub/chapter/Chapter38AnimationActivity.kt
+++ app/src/main/java/com/suein/myapplication/jpub/chapter/Chapter38AnimationActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -17,8 +20,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 /**
@@ -30,10 +35,104 @@ class Chapter38AnimationActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 //            MainScreen()
+            ComponentList()
+        }
+    }
+
+    @Composable
+    fun ComponentList() {
+        val scrollState = rememberScrollState()
+        Column(Modifier.verticalScroll(scrollState)) {
+            MotionsDemo()
+            ColorChangDemo()
             RotateDemo()
         }
     }
 
+
+
+
+    enum class BoxPosition {
+        Start, End
+    }
+
+    /**
+     * animateDpAsState를 이용한 움직임 애니메이션 처리
+     */
+    @Composable
+    fun MotionsDemo() {
+        var boxState by remember { mutableStateOf(BoxPosition.Start) }
+        var screenWidth = (LocalConfiguration.current.screenWidthDp.dp)
+        val boxSideLength = 70.dp
+        val animatedOffset: Dp by animateDpAsState(
+            targetValue = when (boxState) {
+                BoxPosition.Start -> 0.dp
+                BoxPosition.End -> screenWidth - boxSideLength
+            },
+            animationSpec = tween(500)
+        )
+        Column() {
+            Box(
+                modifier = Modifier
+                    .offset(x = animatedOffset, y = 20.dp)
+                    .size(boxSideLength)
+                    .background(Color.Red)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Button(
+            onClick = {
+                boxState = when (boxState) {
+                    BoxPosition.End -> BoxPosition.Start
+                    BoxPosition.Start -> BoxPosition.End
+                }
+            },
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Text(text = "Move Box")
+        }
+    }
+
+
+    /**
+     * animateColorAsSate를 이용한 색상변경 애니메이션 처리
+     */
+    @Composable
+    fun ColorChangDemo() {
+        var colorState by remember { mutableStateOf(BoxColor.Red) }
+        val animatedColor: Color by animateColorAsState(
+            targetValue = when (colorState) {
+                BoxColor.Red -> Color.Magenta
+                BoxColor.Magenta -> Color.Red
+            },
+            animationSpec = tween(4500)
+
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .size(200.dp)
+                    .background(animatedColor)
+            )
+
+            Button(
+                onClick = {
+                    colorState = when (colorState) {
+                        BoxColor.Red -> BoxColor.Magenta
+                        BoxColor.Magenta -> BoxColor.Red
+                    }
+                }) {
+                Text(text = "Change Color")
+            }
+        }
+    }
 
     @Composable
     fun MainScreen() {
@@ -85,32 +184,12 @@ class Chapter38AnimationActivity : ComponentActivity() {
         Red, Magenta
     }
 
-    @Composable
-    fun ColorChangDemo() {
-        var colorState by remember { mutableStateOf(BoxColor.Red) }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Box(
-                modifier = Modifier
-                    .padding(20.dp)
-                    .size(200.dp)
-                    .background(Color.Red)
-            )
-
-            Button(onClick = { /*TODO*/ }) {
-                
-            }
-        }
-    }
-
-
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview() {
 //        MainScreen()
 //        RotateDemo() //클릭시 로테이트 애니메이션 구현
-        ColorChangDemo()
+//        ColorChangDemo() // 클릭시 색 변경하게
+        ComponentList()
     }
 }
